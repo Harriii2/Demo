b2937fec20b7ec58a1caecbc3f03ef74c8d93db5
Hari Thakur <harichandrathakur02@gmail.com>
2025-11-11 23:25:23 +0545

    Update Image URL

--- demo.py
+++ demo.py
@@ -1,58 +1,28 @@
 import requests
 from parsel import Selector
 
-def title(response):
-    for list in response.xpath('//li/article'):
-         title=list.xpath('.//h3/a/text()').get()
-         return title
-        
-
-def price(response):
-    for list in response.xpath('//li/article'):
-         price = list.xpath('.//p[@class="price_color"]/text()').get()
-         return price
-      
-        
-
-def check_avaiablity(response):
-    for list in response.xpath('//li/article'):
-        raw = list.xpath('.//p[@class="instock availability"]/text()').getall()
-        clean =''.join(x.strip() for x in raw if x.strip())
-        return clean
-
-
-def image_url(response):
-    for list in response.xpath('//li/article'):
-        url = list.xpath('.//a/@href').get()
-        return url
-
-
-def rating(response):
-    for list in response.xpath('//li/article'):
-        rate = list.xpath('.//p/@class').get().split(' ')[1]
-        return rate
-        
-
-
-
-for i in range(1,2):
+for i in range(1,3):
     r=requests.get(f'https://books.toscrape.com/catalogue/page-{i}.html')
 
     response=Selector(r.text)
 
-    for item in response.xpath('//li/article'):
-     print(f""" 
-           Title:{title(response)}
-           Price:{price(response)}
-           Availabilty:{check_avaiablity(response)}
-           Rating:{rating(response)}
-           URL:{image_url(response)}
-           """)
+    for list in response.xpath('//li/article'):
+        title=list.xpath('.//h3/a/text()').get()
+        price = list.xpath('.//p[@class="price_color"]/text()').get()
+        stock = list.xpath('.//p[@class="instock availability"]/text()').getall()[1].strip()
+        rating = list.xpath('.//p[contains(@class, "star-rating")]/@class').get().split()[-1]
+        image_url = list.xpath('.//img/@src').get()
+        image_url = image_url.replace('../', '')
 
+        baseurl = 'https://books.toscrape.com/'
+        url = baseurl + image_url
 
-    # title(response)
-    # price(response)
-    # check_avaiablity(response)
+        print(f"Title: {title}")
+        print(f"Price: {price}")
+        print(f'Stock: {stock}')
+        print(f'Rating: {rating}')
+        print(f'Image URL: {url}')
+        print('')
 
     print(f'page {i} complete')
     print(r.status_code)
